6e428f67669bbaf3de24f9093d3a3ed4c4d9eaf5
unknown <sishawon07@gmail.com>
2023-03-05 23:06:43 +0600

    first comit

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const objectID = require("mongodb").ObjectId;
+
+require("dotenv").config();
+const cors = require("cors");
+
 const app = express();
-const port = process.env.port || 7000;
+const port = process.env.PORT || 7000;
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://AsonsServer:shawon646@cluster0.sfpfh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("CarPackage");
+    const carCollection = database.collection("cars");
+
+    // get cars api
+    app.get("/cars", async (req, res) => {
+      const cursor = carCollection.find({});
+      const cars = await cursor.toArray();
+      res.send(cars);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Simple node server");
+  res.send(" car server");
 });
 
 const user = [
